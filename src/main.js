ef1257bf0d74151f6cd840a4e41890eb2e8ec0f9
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-21 20:35:28 +1300

    create scene, camera and renderer

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import{
     PerspectiveCamera,
     MeshStandardMaterial,
     WebGLRenderer,
-} from './vendor/three/build/three.module.js';
-
+    Scene,
+} from 'three';
 import{OrbitControls} from './vendor/three/examples/jsm/controls/OrbitControls.js';
+
+const scene = new Scene();
+const fov = 35; // AKA Field of View
+const aspect = container.clientWidth / container.clientHeight;
+const near = 0.1; // the near clipping plane
+const far = 100; // the far clipping plane
+
+const camera = new PerspectiveCamera(fov, aspect, near, far);
+
+const renderer = new WebGLRenderer();
